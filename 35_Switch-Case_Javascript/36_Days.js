183187a671188f559e46cfdb0b75e3e43b21859b
Pranav Rautrao <pranavrautrao88@gmail.com>
2026-07-21 10:28:33 +0530

    Add JavaScript files for days, players, and months using switch-case statements

---
+++ 35_Switch-Case_Javascript/36_Days.js
@@ -0,0 +1,24 @@
+let day = 3;
+
+switch(day)
+{
+    case 1:
+        console.log("Mondauy");
+        break;
+    
+    case 2:
+        console.log("Tuesday");
+        break;
+        
+    case 3:
+        console.log("wednesday");
+        break;
+        
+    case 4:
+        console.log("Thursday");
+        break;
+        
+    default:
+        console.log("Invalid Day");    
+
+}
---
+++ 35_Switch-Case_Javascript/37_Players.js
@@ -0,0 +1,28 @@
+let player = 2;
+
+switch(player)
+{
+    case 1:
+        console.log("MS Dhoni");
+        break;
+
+    case 2:
+        console.log("Hardik Pandya");
+        break;
+        
+    case 3:
+        console.log("Virat Kohli");
+        break;
+        
+    case 4:
+        console.log("Rohit Sharma");
+        break;
+        
+    case 5:
+        console.log("Suresh Raina");
+        break;
+        
+    default:
+        console.log("Invalid Player Name");
+
+}
