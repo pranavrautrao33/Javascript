fc10f4d76ec6f49edf58f0ba337765f3f86d90e1
Pranav Rautrao <pranavrautrao88@gmail.com>
2026-07-23 08:12:42 +0530

    Add JavaScript file demonstrating various string methods and manipulations

---
+++ 86_String_Methods.js
@@ -0,0 +1,76 @@
+let str= "Pranav Rautrao";
+
+console.log(str.length);
+
+
+let str1 = "Mahi";
+
+console.log(str1.toLocaleUpperCase());
+
+let str2 = "HARDIK";
+
+console.log(str2.toLocaleLowerCase());
+
+let str3 = " Pranav ";
+
+console.log(str3.trim());
+
+
+let str4 = "Lord";
+
+console.log(str4.charAt(2));
+
+let str5 = "Perth";
+
+console.log(str5.at(-2));
+
+let str6 = "Wankhede";
+
+console.log(str6.indexOf("d"));
+
+
+let str7 = "MA Chidambaram";
+
+console.log(str7.lastIndexOf("m")); 
+
+let str8 = "Chanaswami";
+
+console.log(str8.includes("swami"));
+
+let str9 = "Eidan Garden";
+
+console.log(str9.startsWith("Eidan"));
+
+let str10 = "Narendra Modi Stadium";
+
+console.log(str10.endsWith("um"));
+
+let str11 = "Johanbarg Cricket Stadium";
+
+console.log(str11.slice(3,11));
+
+let str12 = "Ricky Ponting";
+console.log(str12.substring(0,7));
+
+let str13 = "Hardik Pandya";
+
+console.log(str13.replace("Hardik","HH"));
+
+let str14 = " World Champion Australia";
+
+console.log(str14.replaceAll("Australia","England"));
+
+let str15 = "Dhoni,Virat,Rohit";
+
+console.log(str15.split(";"));
+
+let a = " Pranav";
+let b = "Rautrao";
+
+console.log(a.concat(" ",b));
+
+
+let str16 = "Hello";
+
+console.log((str16 + " ").repeat(7));
+
